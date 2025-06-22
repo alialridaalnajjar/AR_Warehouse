5b2096f388c5bf79131619cc3695705fd05388a1
Ali Al Rida Al Najjar <alialridaalnajjar@gmail.com>
2025-06-22 15:04:06 +0300

    1.51V feat: restore FilterBar component structure and improve layout

--- src/HrComponents/FilterBar.tsx
+++ src/HrComponents/FilterBar.tsx
@@ -1,9 +1,20 @@
-
 export default function FilterBar() {
   return (
-    <div className="text-white ">
-
-        testttt
+    <div className="text-white">
+     <h1 className="text-xl ml-5  mb-2 font-semibold">Categories</h1>
+      <div className="   border-violet-500/50   rounded-lg border-1 h-fit w-fit mb-5 mx-5 p-1 bg-violet-900/50 flex-wrap  flex flex-row  justify-around   gap-3">
+        <div className="rounded-3xl bg-violet-500 p-1 text-sm">discount</div>
+        <div className="rounded-3xl bg-violet-500 p-1 text-sm">Ai-Related</div>
+        <div className="rounded-3xl bg-violet-500 p-1 text-sm">
+          Tech-Related
+        </div>
+        <div className="rounded-3xl bg-violet-500 p-1 text-sm">Nitendo</div>
+        <div className="rounded-3xl bg-violet-500 p-1 text-sm">Gfuel-PWR</div>
+        <div className="rounded-3xl bg-violet-500 p-1 text-sm">Xbox</div>
+        <div className="rounded-3xl bg-violet-500 p-1 text-sm">Playstation</div>
+        <div className="rounded-3xl bg-violet-500 p-1 text-sm">PC/Parts</div>
+        <div className="rounded-3xl bg-violet-500 p-1 text-sm">Gaming</div>
+      </div>
     </div>
-  )
+  );
 }
